94bd67567ee2fd63722c446a3fdc0acf867903ec
kush_malviya <kushmalviya12345@gmail.com>
2022-05-15 08:30:40 +0530

    print('hello')

--- bikshare.py
+++ bikshare.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import datetime as dt
 import click
-
+print('hello')
 CITY_DATA = {'chicago': 'chicago.csv',
              'new york city': 'new_york_city.csv',
              'washington': 'washington.csv'}
